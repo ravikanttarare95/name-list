89983a619dc25c66813e4c5b683ed6fba9c1a8d2
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-05-08 15:16:36 +0530

    Proper Logic to enter and delete name from the list

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const studentsList = document.getElementById("list");
 const newName = document.getElementById("new-name");
 
 for (let i = 0; i < students.length; i++) {
-    studentsList.innerHTML += `<p>${students[i]} </p`;
+  studentsList.innerHTML += `<p>${students[i]} <img src='./images/delete.png' width="18px" style="cursor:pointer; vertical-align:middle"; onclick="deleteItem(${i})" /></p`;
 }
 
-const deleteIcon = document.getElementById("delete-icon");
-
 function insertName() {
-    studentsList.innerHTML += `<p>${newName.value} </p>`;
+  if (!!newName.value == true) {
+    students.push(newName.value);
     newName.value = "";
+    studentsList.innerHTML = "";
+    for (let i = 0; i < students.length; i++) {
+      studentsList.innerHTML += `<p>${students[i]} <img src='./images/delete.png' width="18px" style="cursor:pointer; vertical-align:middle"; onclick="deleteItem(${i})" /></p`;
+    }
+  } else {
+    alert("Enter your Name:");
+  }
+}
+
+function deleteItem(index) {
+  students.splice(index, 1);
+  studentsList.innerHTML = "";
+  for (let i = 0; i < students.length; i++) {
+    studentsList.innerHTML += `<p>${students[i]} <img src='./images/delete.png' width="18px" style="cursor:pointer; vertical-align:middle"; onclick="deleteItem(${i})" /></p`;
+  }
 }
