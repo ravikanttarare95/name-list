23b58c643f87fc3b4118f9c530569169485a7248
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-05-08 11:41:33 +0530

    First commit with some code

---
+++ script.js
@@ -0,0 +1,12 @@
+const students = ["Ravikant", "Sharad", "Suraj"];
+const studentsList = document.getElementById("list");
+const newName = document.getElementById("new-name");
+
+for (let i = 0; i < students.length; i++) {
+    studentsList.innerHTML += `<p>${students[i]} </p`;
+}
+
+function insertName() {
+    studentsList.innerHTML += `<p>${newName.value} </p>`;
+    newName.value = "";
+}
