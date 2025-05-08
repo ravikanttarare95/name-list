d9ef7c3b25eca62936397ebf75eb82ad3530cd45
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-05-08 16:28:08 +0530

    Moodification in UI

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ const newName = document.getElementById("new-name");
 showList();
 function showList() {
   for (let i = 0; i < students.length; i++) {
-    studentsList.innerHTML += `<p>${students[i]} <img src='./images/delete.png' width="18px" style="cursor:pointer; vertical-align:middle"; onclick="deleteItem(${i})" /></p`;
+    studentsList.innerHTML += `<div style="margin:15px 0">  <div style="display:flex;justify-content:space-between;  background-color: #e9e9e9;padding:10px; border-radius:3px">${
+      i + 1
+    }. ${students[i]} 
+  <div>
+    <img src='./images/edit.png' width="18px" style="cursor:pointer;vertical-align:middle"; onclick="editItem(${i})"/> 
+    <img src='./images/delete.png' width="18px" style="cursor:pointer; vertical-align:middle"; onclick="deleteItem(${i})" />
+     </div>     </div>
+      </div> `;
   }
 }
 
@@ -21,6 +28,12 @@ function insertName() {
 
 function deleteItem(index) {
   students.splice(index, 1);
+  studentsList.innerHTML = "";
+  showList();
+}
+
+function editItem(index) {
+  students.splice(index, 1, "Edit");
   studentsList.innerHTML = "";
   showList();
 }
