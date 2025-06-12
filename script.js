5eab0ddd70cea81982beac0c48c7c2af9df0e625
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-06-12 18:04:24 +0530

    Optimized Code and some styling in edit box

--- script.js
+++ script.js
@@ -1,57 +1,95 @@
 const students = [];
 
+//5
+
+function loadFromLStorage() {
+  const studentsName = JSON.parse(localStorage.getItem("studentsName")) || [];
+  students.push(...studentsName); //... Spread Operator pushes all items one by one. Also, instead of doing this, we can directly replace studentsName with students in above line.BUT we have to do it outside this function.
+  renderList();
+}
+
+loadFromLStorage();
+
 const studentsList = document.getElementById("list-container");
-const newName = document.getElementById("new-name");
-const newNumber = document.getElementById("new-number");
 const mainImg = document.getElementById("main-img");
+const addBtn = document.getElementById("add-icon");
 
-function dataFromLocalStorage() {
-  const studentsName = JSON.parse(localStorage.getItem("StudentsName"));
-  if (studentsName) {
-    students.push(...studentsName);
-  }
-  if (students.length === 0) {
-    const noWorkPara = document.getElementById("no-student-para");
-    noWorkPara.innerText = "No Students";
-    mainImg.src = "./images/happy.png";
-  } else {
-    showList();
-  }
-}
-dataFromLocalStorage();
+addBtn.addEventListener("click", insertName);
 
-function showList() {
-  localStorage.setItem("StudentsName", JSON.stringify(students));
-  studentsList.innerHTML = "";
-  for (let i = 0; i < students.length; i++) {
-    studentsList.innerHTML += `<div style="margin:15px 0">  <div style="display:flex;justify-content:space-between;  background-color: #e9e9e9;padding:10px; border-radius:3px">${
-      i + 1
-    }. ${students[i]} 
-  <div>
-    <img src='./images/edit.png' width="18px" style="cursor:pointer;vertical-align:middle"; onclick="editItem(${i})"/> 
-    <img src='./images/delete.png' width="18px" style="cursor:pointer; vertical-align:middle"; onclick="deleteItem(${i})" />
-     </div>     </div>
-      </div> `;
-  }
-}
+//1
+const nameInputElement = document.getElementById("new-name");
 
 function insertName() {
-  if (!!newName.value == true) {
-    students.unshift(newName.value);
-    showList();
-    newName.value = "";
+  const newName = nameInputElement.value;
+  const newNumber = document.getElementById("new-number").value;
+
+  students.push(newName);
+  saveToLStorage();
+  nameInputElement.value = "";
+  renderList();
+}
+
+//2
+function renderList() {
+  const studentsList = document.getElementById("list-container");
+  studentsList.innerHTML = "";
+  if (students.length !== 0) {
+    for (let i = 0; i < students.length; i++) {
+      studentsList.innerHTML += `
+            <div class="list-item">
+                      <p>${students[i]}</P>
+                      <div>
+                          <img src="./images/edit.png" class="edit-icon" onclick="editName(${i})">
+                          <img src="./images/delete.png" class="delete-icon" onclick="deleteName(${i})">
+                      </div>
+
+            </div>
+            `;
+    }
   } else {
-    alert("Enter your Name:");
+    studentsList.innerHTML += `
+                 <p id="no-student-para"></p>
+            <img src="./images/happy.png" id="main-img" alt="Image" width="100%">
+     `;
   }
 }
+renderList();
 
-function deleteItem(index) {
-  students.splice(index, 1);
-  showList();
+//3
+// const deleteIcon = document.getElementById("delete-icon");
+// deleteIcon.addEventListener("click", deleteName);
+function deleteName(i) {
+  students.splice(i, 1);
+  saveToLStorage();
+  renderList();
 }
 
-// function editItem(index) {
-//   students.splice(index, 1, students[index]);
-//   studentsList.innerHTML = "";
-//   showList();
-// }
+//4
+
+function saveToLStorage() {
+  localStorage.setItem("studentsName", JSON.stringify(students));
+}
+
+//6
+
+function editName(i) {
+  const currentName = students[i];
+
+  const editingDiv = document.createElement("div");
+  editingDiv.innerHTML = `
+  <div class="editing-list-item">
+      <input type="text" class="editingInput" value="${currentName}"/>
+      <button class="btn-save-edit" onclick="saveEdit(${i})">Save</button>
+  </div>
+  `;
+
+  const allStudents = studentsList.children; // If we use .children  then only we can access children's Index
+  studentsList.replaceChild(editingDiv, allStudents[i]); // removing allStudents[i] & adding NEW div editingDiv
+}
+
+function saveEdit(i) {
+  const editingInput = studentsList.children[i].querySelector(".editingInput");
+  students.splice(i, 1, editingInput.value);
+  saveToLStorage();
+  renderList();
+}
